bf24b75911d0c81aadf1ef982e4f213b40e5cd5a
baatrox-s4ma <saadmea010@gmail.com>
2026-04-13 02:05:46 +0100

    adding lsp restart

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 require("baatrox")
 
-vim.keymap.set("i", "jk", "<esc>");
-vim.keymap.set("i", "JK", "<esc>");
+vim.keymap.set("n", "<leader>lr", ":lsp restart<CR>", { desc = "Restart LSP" })
+vim.keymap.set("i", "jk", "<esc>")
+vim.keymap.set("i", "JK", "<esc>")
 
 vim.keymap.set("n", "<space>e", ":20Lexplore<CR>")
 
-
 vim.keymap.set("n", "<C-h>", ":bp<CR>")
 vim.keymap.set("n", "<C-l>", ":bp<CR>")
-vim.keymap.set("n", "<C-q>", ":bd<CR>", {noremap=true})
+vim.keymap.set("n", "<C-q>", ":bd<CR>", { noremap = true })
 
-vim.opt.number=true;
-vim.opt.relativenumber=true;
+vim.opt.number = true
+vim.opt.relativenumber = true
